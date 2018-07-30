31093c403c75bf0354181ef1837737c6f4ec492b
Mrmrprofessional <michael.rapp358@gmail.com>
2018-07-30 14:25:40 -0700

    set ad_hoc_variant_exclusion to save id

--- app/controllers/spree/admin/ad_hoc_variant_exclusions_controller.rb
+++ app/controllers/spree/admin/ad_hoc_variant_exclusions_controller.rb
@@ -6,12 +6,12 @@ module Spree
     def create
       return unless params["ad_hoc_option_type"]
       ave = AdHocVariantExclusion.new
-      params["ad_hoc_option_type"].each_pair do |otid, ovid|
-        next if ovid.empty?
-        eov = ExcludedAdHocOptionValue.create(ad_hoc_variant_exclusion: ave, ad_hoc_option_value_id: ovid)
-      end
       ave.product = @product
       ave.save
+      params["ad_hoc_option_type"].each_pair do |otid, ovid|
+        next if ovid.empty?
+        eov = ExcludedAdHocOptionValue.create(ad_hoc_variant_exclusion: ave.id, ad_hoc_option_value_id: ovid)
+      end
       redirect_to admin_product_ad_hoc_variant_exclusions_path(@product)
     end
 
