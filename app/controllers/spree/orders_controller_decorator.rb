d2fa3a3685fb4497dc156229d4e538cb4f1050e6
Mrmrprofessional <michael.rapp358@gmail.com>
2018-08-06 13:11:04 -0700

    removed populate

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -7,36 +7,6 @@ module Spree
     before_action :set_option_params_values, only: [:populate]
     before_action :check_variant_exclusions, only: [:populate]
 
-    # Adds a new item to the order (creating a new order if none already exists)
-    def populate
-      order    = current_order(create_order_if_necessary: true)
-      variant  = Spree::Variant.find(params[:variant_id])
-      quantity = params[:quantity].to_i
-      options  = params[:options] || {}
-
-      # 2,147,483,647 is crazy. See issue #2695.
-      if quantity.between?(1, 2_147_483_647)
-        begin
-          order.contents.add(variant, quantity, options)
-          order.create_tax_charge!
-          order.update_with_updater!
-        rescue ActiveRecord::RecordInvalid => e
-          error = e.record.errors.full_messages.join(', ')
-        end
-      else
-        error = Spree.t(:please_enter_reasonable_quantity)
-      end
-
-      if error
-        flash[:error] = error
-        redirect_back_or_default(spree.root_path)
-      else
-        respond_with(order) do |format|
-          format.html { redirect_to(cart_path(variant_id: variant.id)) }
-        end
-      end
-    end
-
     private
 
     def set_option_params_values
